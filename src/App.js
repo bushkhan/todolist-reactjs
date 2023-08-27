92e2c87ee2f9d80ca62252c788af10d66da256bf
Bushra Khan <bushrakhan2212002@gmail.com>
2023-08-27 23:11:24 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -2,17 +2,29 @@ import Header from "./components/Header";
 import Footer from "./components/Footer";
 import Note from "./components/Note";
 import CreateArea from "./components/CreateArea";
+import { useState } from "react";
 
 
 function App() {
+  const [notes, setNotes] = useState([]);
+  function addNote(note) {
+    setNotes(prevNotes => {
+      return [...prevNotes, note]
+    });
+  }
   return (
     <div>
-      <Header/>
-      <CreateArea/>
-      <Note key={1} title="Note Title" content="Note content"/>
-      <Footer/>
+      <Header />
+      <CreateArea onAdd={addNote} />
+      {
+        notes.map((item) => {
+          return <Note key={1} title={item.title} content={item.content} />
+        })
+      }
+
+      <Footer />
     </div>
-   
+
   );
 }
 
--- src/components/CreateArea.jsx
+++ src/components/CreateArea.jsx
@@ -1,12 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
 
-function CreateArea() {
+function CreateArea(props) {
+
+    const [note, setNotes] = useState({
+        title:"",
+        content:""
+    });
+
+
+    function getInput(event){
+        const {name, value} = event.target
+        setNotes((preVal)=>{
+            return { 
+                ...preVal,
+                [name]: value
+            }
+        });
+    
+    }
+
+    function submitNote(event){
+        props.onAdd(note);
+        event.preventDefault();
+
+    }   
   return (
     <div>
       <form>
-        <input name="title" placeholder="Title" />
-        <textarea name="content" placeholder="Take a note..." rows="3" />
-        <button>Add</button>
+        <input onChange={getInput} value={note.title} name="title" placeholder="Title" />
+        <textarea  onChange={getInput} value={note.content} name="content" placeholder="Take a note..." rows="3" />
+        <button onClick={submitNote}>Add</button>
       </form>
     </div>
   );
